de61d232a181fccdf8d7675b17cdc4f268f6d8d1
Nathan Cutler <ncutler@suse.com>
2017-07-13 23:09:11 +0200

    task: syslog: salt INFO messages generate false negatives
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- teuthology/task/internal/syslog.py
+++ teuthology/task/internal/syslog.py
@@ -128,6 +128,8 @@ def syslog(ctx, config):
                     run.Raw('|'),
                     'grep', '-v', 'ceph-create-keys: INFO',
                     run.Raw('|'),
+                    'grep', '-v', '\\bsalt-master\\b|\\bsalt-minion\\b',
+                    run.Raw('|'),
                     'head', '-n', '1',
                 ],
                 stdout=StringIO(),
